922f2800f80444268dbb2564898ff1f810844c01
dassoumik <74944139+dassoumik@users.noreply.github.com>
2021-05-10 11:15:23 -0400

    create a navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Home from './pages/Home/Home.js';
+import Product from './pages/Product/Product.js';
+
 
 
 function App() {
@@ -8,6 +10,7 @@ function App() {
     <div className="App">
       <Switch> 
         <Route exact path="/" component={Home}/>
+        <Route exact path="/Product" component={Product}/>
       </Switch>
     </div>
   );
--- client/src/pages/Product/Product.js
+++ client/src/pages/Product/Product.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Navbar from '../../components/Navbar';
+
 
 function Product() {
     return (
         <div>
-            
+            <Navbar />
         </div>
     )
 }
