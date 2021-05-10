6da8b60cb1356ced2088b8667287bed8448808f5
dassoumik <74944139+dassoumik@users.noreply.github.com>
2021-05-10 11:34:28 -0400

    create the initial grid layout for products

---
+++ client/src/components/ProductItem/ProductItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ProductItem() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ProductItem
--- client/src/pages/Product/Product.js
+++ client/src/pages/Product/Product.js
@@ -1,11 +1,47 @@
 import React from 'react'
 import Navbar from '../../components/Navbar';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import ProductItem from '../../components/ProductItem/ProductItem'
 
 
 function Product() {
+    const classes = makeStyles((theme) => ({
+        root: {
+          flexGrow: 1,
+        },
+        paper: {
+          padding: theme.spacing(2),
+          textAlign: 'center',
+          color: theme.palette.text.secondary,
+        },
+      }));
+
     return (
         <div>
             <Navbar />
+            <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={0} sm={12}>
+          <Paper className={classes.paper}>sm=12</Paper>
+        </Grid>
+      </Grid>
+      <Grid container spacing={3}>
+        <Grid item xs={0} sm={3}>
+          <Paper className={classes.paper}>sm=3</Paper>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Paper className={classes.paper}>
+              sm=6
+              <ProductItem/>
+          </Paper>
+        </Grid>
+        <Grid item xs={0} sm={3}>
+          <Paper className={classes.paper}>sm=3</Paper>
+        </Grid>
+      </Grid>
+        </div>
         </div>
     )
 }
