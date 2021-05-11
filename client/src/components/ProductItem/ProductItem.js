66d80182516f1c6ed423de08844131688d81bcd4
dassoumik <74944139+dassoumik@users.noreply.github.com>
2021-05-11 05:35:55 -0400

    add basic card features

--- client/src/components/ProductItem/ProductItem.js
+++ client/src/components/ProductItem/ProductItem.js
@@ -1,62 +1,120 @@
 import React from 'react';
-import productData from "../../util/Api";
 import { makeStyles } from '@material-ui/core/styles';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
 import IconButton from '@material-ui/core/IconButton';
 import StarBorderIcon from '@material-ui/icons/StarBorder';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+// import AddShoppingCartIcon from '@material-ui/core/Button';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+
+import './ProductItem.css'
+
 // import tileData from './tileData';
 
-function ProductItem() {
-    const useStyles = makeStyles((theme) => ({
+function ProductItem({productData}) {
+    const useStyles = makeStyles({
         root: {
-          display: 'flex',
-          flexWrap: 'wrap',
-          justifyContent: 'space-around',
-          overflow: 'hidden',
-          backgroundColor: theme.palette.background.paper,
+          minWidth: 200,
+          maxHeight: 500,
+          margin: 20,
+          backgroundColor: '#3d4a5d',
+          backgroundImage: 'linearGradient(to right, #121212, #3d4a5d)',
+          color: 'white'
         },
-        gridList: {
-          flexWrap: 'nowrap',
-          // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
-          transform: 'translateZ(0)',
-        },
-        title: {
-          color: theme.palette.primary.light,
-        },
-        titleBar: {
-          background:
-            'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
-        },
-      }));
+      });
 
-    // function SingleLineGridList() {
+    //   function ImgMediaCard({productData})  {
         const classes = useStyles();
       
-    return (
-        <div className={classes.root}>
-        <GridList className={classes.gridList} cols={2.5}>
-          {productData.map((tile) => (
-            <GridListTile key={tile.image}>
-              <img src={tile.img} alt={tile.title} />
-              <GridListTileBar
-                title={tile.title}
-                classes={{
-                  root: classes.titleBar,
-                  title: classes.title,
-                }}
-                actionIcon={
-                  <IconButton aria-label={`star ${tile.title}`}>
-                    <StarBorderIcon className={classes.title} />
-                  </IconButton>
-                }
+        return (
+          <Card classNraised="true" >
+            <CardActionArea className={"custom-card"}>
+              <CardMedia
+                component="img"
+                alt="product"
+                height="260"
+                image={productData.image}
+                title={productData.title}
               />
-            </GridListTile>
-          ))}
-        </GridList>
-      </div>
-    )
+              <CardContent className="content custom-card">
+                <Typography className='title' gutterBottom variant="h6" component="p">
+                  {productData.title}
+                </Typography>
+                <Typography className='basket-item-price' variant="body2" color="white" component="p">
+                  Price ${productData.price}
+                </Typography>
+              </CardContent>
+              <CardActions className="custom-card actions-content">
+            <Button className="custom-button" color="red" aria-label="add to shopping cart">
+                <AddShoppingCartIcon/>
+            </Button>
+              </CardActions>
+            </CardActionArea>
+            {/* <CardActions> */}
+              {/* <Button size="small" color="primary">
+                Learn More
+              </Button> */}
+            {/* </CardActions> */}
+          </Card>
+        );
+      
+
+    // const useStyles = makeStyles((theme) => ({
+    //     root: {
+    //       display: 'flex',
+    //       flexWrap: 'wrap',
+    //       justifyContent: 'space-around',
+    //       overflow: 'hidden',
+    //       backgroundColor: theme.palette.background.paper,
+    //     },
+    //     gridList: {
+    //       flexWrap: 'nowrap',
+    //       // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+    //       transform: 'translateZ(0)',
+    //     },
+    //     title: {
+    //       color: theme.palette.primary.light,
+    //     },
+    //     titleBar: {
+    //       background:
+    //         'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+    //     },
+    //   }));
+
+    // // function SingleLineGridList() {
+    //     const classes = useStyles();
+      
+    // return (
+    //     <div className={classes.root}>
+    //     <GridList className={classes.gridList} cols={2.5}>
+    //       {productData.map((tile) => (
+    //         <GridListTile key={tile.id}>
+    //           <img src={tile.image} alt={tile.title} />
+    //           <GridListTileBar
+    //             title={tile.title}
+    //             classes={{
+    //               root: classes.titleBar,
+    //               title: classes.title,
+    //             }}
+    //             actionIcon={
+    //               <IconButton aria-label={`star ${tile.title}`}>
+    //                 <StarBorderIcon className={classes.title} />
+    //               </IconButton>
+    //             }
+    //           />
+    //         </GridListTile>
+    //       ))}
+    //     </GridList>
+    //   </div>
+    // )
     }
 // }
 
--- client/src/pages/Product/Product.js
+++ client/src/pages/Product/Product.js
@@ -3,7 +3,9 @@ import Navbar from '../../components/Navbar';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import ProductItem from '../../components/ProductItem/ProductItem'
+import ProductItem from '../../components/ProductItem/ProductItem';
+import productData from "../../util/Api";
+
 
 
 function Product() {
@@ -13,34 +15,55 @@ function Product() {
         },
         paper: {
           padding: theme.spacing(2),
-          textAlign: 'center',
+          textAlign: 'left',
           color: theme.palette.text.secondary,
+          margin: 3,
+          backgroundColor: '#3d4a5d',
+          backgroundImage: 'linearGradient(to right, #121212, #3d4a5d)',
+          display: 'flex',
+          flexDirection: 'row',
+          overflow: 'hidden'
         },
+        container: {
+          padding: "5%"
+        }
       }));
 
     return (
         <div>
             <Navbar />
             <div className={classes.root}>
-      <Grid container spacing={3}>
+      <Grid container className={classes.container} spacing={4} direction="column">          
+      <Grid container spacing={4}>
         <Grid item xs={0} sm={12}>
-          <Paper className={classes.paper}>sm=12</Paper>
+          <Paper className={classes.paper}>What is Lorem Ipsum?
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+Why do we use it?
+It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+
+</Paper>
         </Grid>
-      </Grid>
       <Grid container spacing={3}>
-        <Grid item xs={0} sm={3}>
+        <Grid item xs={0} sm={3} md={1}>
           <Paper className={classes.paper}>sm=3</Paper>
         </Grid>
-        <Grid item xs={12} sm={6}>
-          <Paper className={classes.paper}>
-              sm=6
-              <ProductItem/>
+      <Grid container className={classes.container} xs={12} sm={6} md={9} spacing={8}>
+             { productData.map((productData) => 
+        <Grid item xs={12} sm={3} md={4}>
+          <Paper  className={classes.paper}>
+              <ProductItem productData={productData}/>
           </Paper>
         </Grid>
-        <Grid item xs={0} sm={3}>
+              )}
+        </Grid>
+        <Grid item xs={0} sm={3} md={2}>
           <Paper className={classes.paper}>sm=3</Paper>
         </Grid>
+        </Grid>
+        </Grid>
       </Grid>
+
         </div>
         </div>
     )
