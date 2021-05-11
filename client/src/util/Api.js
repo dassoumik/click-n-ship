157dbd1645ae5bc6b8dbb202edfc3da7e1c6320c
dassoumik <74944139+dassoumik@users.noreply.github.com>
2021-05-11 05:35:22 -0400

    add basic card features

---
+++ client/src/util/Api.js
@@ -0,0 +1,43 @@
+const productData = [{
+    id: 1,
+    title: "2021 Newest Lenovo IdeaPad 3 14'",
+    image: "https://m.media-amazon.com/images/I/81uu-C7AX0L._AC_UY218_.jpg",
+    price: "619.00",
+    category: "Laptops",
+    rating: "5",
+    numOfRating: "15"
+}, {
+    id: 2,
+    title: "2020 Newest Dell Inspiron 15 5000",
+    image: "https://m.media-amazon.com/images/I/71lhpu15m7L._AC_UY218_.jpg",
+    price: "979.00",
+    category: "Laptops",
+    rating: "4.5",
+    numOfRating: "49"
+}, {
+    id: "",
+    title: "ASUS Laptop L210 Ultra Thin Laptop",
+    image: "https://m.media-amazon.com/images/I/810BY5U9baL._AC_UY218_.jpg",
+    price: "219.99",
+    category: "Laptops",
+    rating: "4.5",
+    numOfRating: "2074"
+}, {
+    id: 4,
+    title: "2021 Newest Dell Inspiron 3000 Laptop",
+    image: "https://m.media-amazon.com/images/I/71pSMhue5cL._AC_UY218_.jpg",
+    price: "539.00",
+    category: "Laptops",
+    rating: "4.5",
+    numOfRating: "52"
+}, {
+    id: 5,
+    title: "Acer Aspire 5 Slim Laptop",
+    image: "https://m.media-amazon.com/images/I/71vvXGmdKWL._AC_UY218_.jpg",
+    price: "432.00",
+    category: "Laptops",
+    rating: "4.0",
+    numOfRating: "30173"
+}];
+
+export default productData;
