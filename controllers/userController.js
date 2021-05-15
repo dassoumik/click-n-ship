dd3995e9fa82a9c9843ab685a5d6e9105fde2d67
Sammy McMillan <74073409+smcmillan28@users.noreply.github.com>
2021-05-15 11:37:35 -0400

    initial controller files setup

---
+++ controllers/userController.js
@@ -0,0 +1,11 @@
+const db = require("../models");
+
+// define route methods for user controller
+module.exports = {
+    create: function(req, res) {
+        db.User
+            .create(req.body)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    }
+}
