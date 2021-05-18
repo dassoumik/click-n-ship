0dae18dc4440dba4ad0b0bb518a3882a902be88a
dassoumik <74944139+dassoumik@users.noreply.github.com>
2021-05-18 06:40:26 -0400

    remove commented codes

--- client/src/components/ProductItem/ProductItem.js
+++ client/src/components/ProductItem/ProductItem.js
@@ -12,13 +12,10 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-// import AddShoppingCartIcon from '@material-ui/core/Button';
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 import {useCountContext} from '../../util/Store';
-
 import './ProductItem.css'
 
-// import tileData from './tileData';
 
 function ProductItem({productData}) {
     const useStyles = makeStyles({
@@ -35,7 +32,6 @@ function ProductItem({productData}) {
 
     const [state, dispatch] = useCountContext();
     
-    //   function ImgMediaCard({productData})  {
         const classes = useStyles();
       
         return (
@@ -62,64 +58,9 @@ function ProductItem({productData}) {
             </Button>
               </CardActions>
             </CardActionArea>
-            {/* <CardActions> */}
-              {/* <Button size="small" color="primary">
-                Learn More
-              </Button> */}
-            {/* </CardActions> */}
           </Card>
         );
       
-
-    // const useStyles = makeStyles((theme) => ({
-    //     root: {
-    //       display: 'flex',
-    //       flexWrap: 'wrap',
-    //       justifyContent: 'space-around',
-    //       overflow: 'hidden',
-    //       backgroundColor: theme.palette.background.paper,
-    //     },
-    //     gridList: {
-    //       flexWrap: 'nowrap',
-    //       // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
-    //       transform: 'translateZ(0)',
-    //     },
-    //     title: {
-    //       color: theme.palette.primary.light,
-    //     },
-    //     titleBar: {
-    //       background:
-    //         'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
-    //     },
-    //   }));
-
-    // // function SingleLineGridList() {
-    //     const classes = useStyles();
-      
-    // return (
-    //     <div className={classes.root}>
-    //     <GridList className={classes.gridList} cols={2.5}>
-    //       {productData.map((tile) => (
-    //         <GridListTile key={tile.id}>
-    //           <img src={tile.image} alt={tile.title} />
-    //           <GridListTileBar
-    //             title={tile.title}
-    //             classes={{
-    //               root: classes.titleBar,
-    //               title: classes.title,
-    //             }}
-    //             actionIcon={
-    //               <IconButton aria-label={`star ${tile.title}`}>
-    //                 <StarBorderIcon className={classes.title} />
-    //               </IconButton>
-    //             }
-    //           />
-    //         </GridListTile>
-    //       ))}
-    //     </GridList>
-    //   </div>
-    // )
-    }
-// }
+  }
 
 export default ProductItem
