a949ef8124fecee16dbd7596a5d0a98b7cc5fc0c
Sammy McMillan <74073409+smcmillan28@users.noreply.github.com>
2021-05-22 10:36:36 -0400

    edited order model

--- models/order.js
+++ models/order.js
@@ -2,13 +2,14 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const orderSchema = new Schema({
-    numberOfOrders: { type: String, required: true },
+    userEmail: { type: String, required: true },
+    orderId: { type: Number, required: true },
     totalPrice: { type: Number, required: true },
-    products: { type: Schema.ObjectId, ref: "Product" },
+    products: { type: Array, required: true },
     date: { type: Date, default: Date.now }
 });
 
 const Order = mongoose.model("Order", orderSchema);
 
 
-module.exports = Order;
+module.exports = Order; 
